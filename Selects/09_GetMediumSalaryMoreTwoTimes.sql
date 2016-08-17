5364f9ee3b21769d57527664299a11e6ab8ff1d6
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:39:23 +0300

    Add inner

--- Selects/09_GetMediumSalaryMoreTwoTimes.sql
+++ Selects/09_GetMediumSalaryMoreTwoTimes.sql
@@ -4,7 +4,7 @@ SELECT
  e.LastName, 
  AVG(s.Salary) MidleSalary
 FROM [Employee] e
-JOIN [Salary] s ON s.EmployeeId = e.EmployeeId
+INNER JOIN [Salary] s ON s.EmployeeId = e.EmployeeId
 WHERE s.Year = 2015
 GROUP BY 
  e.EmployeeId, 
