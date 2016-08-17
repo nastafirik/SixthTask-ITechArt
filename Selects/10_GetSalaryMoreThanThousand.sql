e6f6007d184e11e3dbcface3549303345b09ff2d
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:41:32 +0300

    1

--- Selects/10_GetSalaryMoreThanThousand.sql
+++ Selects/10_GetSalaryMoreThanThousand.sql
@@ -3,7 +3,7 @@ SELECT
  e.FirstName, 
  s.Salary
 FROM [Employee] e
-INNER JOIN [Salary] s ON e.EmployeeId = s.EmployeeId 
+ INNER JOIN [Salary] s ON e.EmployeeId = s.EmployeeId 
 WHERE s.Month=1 
  AND s.Year=2015 
  AND s.Salary>1000
