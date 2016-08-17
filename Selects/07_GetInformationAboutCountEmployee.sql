fd7857a3b5c8e648eb9361f683bf1b76c656e169
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:32:08 +0300

    Add records from table for test

--- Selects/07_GetInformationAboutCountEmployee.sql
+++ Selects/07_GetInformationAboutCountEmployee.sql
@@ -1,6 +1,12 @@
-SELECT j.IdJob, j.JobTitle, (
-							SELECT COUNT(c.IdCareer) 
-							FROM [Career] c
-							WHERE c.IdJob = j.IdJob AND c.DateEnd IS NULL) 
-CountOfEmployees
+SELECT 
+ j.JobId, 
+ j.JobTitle, 
+ (
+  SELECT 
+   COUNT(c.CareerId) 
+  FROM [Career] c
+  WHERE c.JobId = j.JobId 
+   AND c.DateEnd IS NULL
+ ) 
+ CountOfEmployees
 FROM [Job] j
