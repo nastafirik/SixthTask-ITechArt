78328d5073de0c9ffe377341a13f51a8aa9a9630
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:23:33 +0300

    Fix subquery

--- Selects/04_GetOldEmployee.sql
+++ Selects/04_GetOldEmployee.sql
@@ -1,4 +1,16 @@
-SELECT TOP(1) FirstName,DATEDIFF(YEAR, DateBorn, GETDATE()) Old
+SELECT 
+ TOP(1) FirstName,
+ DATEDIFF(YEAR, DateOfBirth, GETDATE()) Old
 FROM Employee
-ORDER BY DateBorn ASC
+ORDER BY DateOfBirth ASC
+GO
 
+SELECT TOP (1)
+ FirstName,
+ DATEDIFF(YEAR, DateOfBirth, GETDATE()) Old
+FROM Employee
+WHERE DateOfBirth = (
+					SELECT 
+					 Min(DateOfBirth) 
+					FROM Employee)
+GO
