7bf8a7c07c7c2a0a83bf004780d1fb02058b2a65
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:28:34 +0300

    Add query

--- Selects/06_GetSalaryFromMay.sql
+++ Selects/06_GetSalaryFromMay.sql
@@ -1,6 +1,19 @@
-SELECT IdEmployee
-FROM Employee
-WHERE IdEmployee in(
-    SELECT IdEmployee
-	FROM Salary
-	WHERE Month=5)
+SELECT Salary.EmployeeId
+FROM Salary
+WHERE Month = 5 
+ AND Year = 2015 
+ AND Salary <
+		(SELECT top(1) s.Salary
+		 FROM Salary s
+		 WHERE s.EmployeeId = Salary.EmployeeId 
+		  AND Month < 5 
+		  AND Year = 2015)
+
+
+
+
+
+
+
+
+
