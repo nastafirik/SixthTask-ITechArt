07b9d8279ca67bb63d3e9a0320d5fceeea51605f
Anastasiya <nastafirik@gmail.com>
2016-08-17 14:54:12 +0300

    6

--- Selects/06_GetSalaryFromMay.sql
+++ Selects/06_GetSalaryFromMay.sql
@@ -1,10 +1,12 @@
-SELECT Salary.EmployeeId
+SELECT 
+ Salary.EmployeeId
 FROM Salary
 WHERE Month = 5 
  AND Year = 2015 
  AND Salary <
-		(SELECT top(1) s.Salary
-		 FROM Salary s
-		 WHERE s.EmployeeId = Salary.EmployeeId 
-		  AND Month < 5 
-		  AND Year = 2015)
+	(SELECT 
+	 top(1) s.Salary
+	FROM Salary s
+	WHERE s.EmployeeId = Salary.EmployeeId 
+	 AND Month < 5 
+	 AND Year = 2015)
